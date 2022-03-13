3f626ba366160688ec49911f514e4e775da5e64c
Swarup9437 <71583930+Swarup9437@users.noreply.github.com>
2022-03-13 11:49:25 +0530

    Update Games.java

--- Games.java
+++ Games.java
@@ -8,24 +8,28 @@ public class Games {
         System.out.println("2. Paper");
         System.out.println("3. Scissor");
         System.out.println("(Choose either 1 or 2 or 3): ");
-        String [] arr = {"Rock", "Paper", "Scissor" };
+        String[] arr = {"Rock", "Paper", "Scissor" };
         int num = sc.nextInt();
-        System.out.println("You: " + arr[num-1]);
+        if (num > 3) {
+            System.out.println("You are out of contest");
+        } else {
+            System.out.println("You: " + arr[num - 1]);
 
-        Random randomChoice = new Random();
-        int idx = randomChoice.nextInt(3);
-        switch (idx) {
-            case 0 -> System.out.println("Computer: Rock");
-            case 1 -> System.out.println("Computer: Paper");
-            case 2 -> System.out.println("Computer: Scissor");
+            Random randomChoice = new Random();
+            int idx = randomChoice.nextInt(3);
+            switch (idx) {
+                case 0 -> System.out.println("Computer: Rock");
+                case 1 -> System.out.println("Computer: Paper");
+                case 2 -> System.out.println("Computer: Scissor");
+            }
+
+            if (num - 1 == idx) System.out.print("Same Pinch");
+            else if (num == 1 && idx == 1) System.out.print("Computer Win");
+            else if (num == 1 && idx == 2) System.out.print(" BOOM You Win");
+            else if (num == 2 && idx == 0) System.out.print(" BOOM You Win");
+            else if (num == 2 && idx == 2) System.out.print("Computer Win");
+            else if (num == 3 && idx == 0) System.out.print("Computer Win");
+            else System.out.print(" BOOM You Win"); // (num == 3 && idx == 1)
         }
-
-        if (num-1 == idx) System.out.print("Same Pinch");
-        else if (num == 1 && idx == 1) System.out.print("Computer Win");
-        else if (num == 1 && idx == 2) System.out.print(" BOOM You Win");
-        else if (num == 2 && idx == 0) System.out.print(" BOOM You Win");
-        else if (num == 2 && idx == 2) System.out.print("Computer Win");
-        else if (num == 3 && idx == 0) System.out.print("Computer Win");
-        else System.out.print(" BOOM You Win"); // (num == 3 && idx == 1)
     }
 }
